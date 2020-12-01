30d4ed1fa4af95b916e74d4204783c0bbacb4298
Edmand46 <theedison4@gmail.com>
2020-12-01 11:22:17 +0300

    Redirect support

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const http = require('https');
+const url = require('url');
 const ffmpeg = require('fluent-ffmpeg');
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
 const ffprobePath = require('@ffprobe-installer/ffprobe').path;
@@ -15,8 +16,28 @@ const downloadFile = (url, savePath) => new Promise((resolve, reject) => {
     console.log('End download file');
     resolve();
   });
-  http.get(url, response => response.pipe(fileHandle));
+
+  httpGet(url, fileHandle);
 });
+
+function httpGet(resourceUrl, outStream) {
+  const {host, protocol} = url.parse(resourceUrl);
+  http.get(resourceUrl, response => {
+    const redirectLocation = response.headers.location;
+    if (redirectLocation) {
+      if (redirectLocation.match(/^http/)) {
+        console.log('Redirection to: ' + redirectLocation);
+        httpGet(redirectLocation, outStream);
+      } else {
+        const relativeRedirectLocation = `${protocol}//${host}${redirectLocation}`;
+        console.log('Redirection to: ' + relativeRedirectLocation);
+        httpGet(relativeRedirectLocation, outStream);
+      }
+    } else {
+      response.pipe(outStream)
+    }
+  });
+}
 
 const readFile = filePath => new Promise((resolve, reject) => fs.readFile(filePath, ((err, data) => {
   if (err) reject(err);
@@ -24,14 +45,14 @@ const readFile = filePath => new Promise((resolve, reject) => fs.readFile(filePa
 })));
 
 const convertFile = (parameters) => new Promise((resolve, reject) => {
-  const { input, output, logging, resize } = parameters;
+  const {input, output, logging, resize} = parameters;
   let task = ffmpeg(input).format('mp4');
 
   if (resize)
     task = task.addOutputOption('-vf', 'scale=w=400:h=400:force_original_aspect_ratio=2,crop=400:400');
 
   task.on('end', resolve)
-    .on('progress', ({ percent }) => logging(percent))
+    .on('progress', ({percent}) => logging(percent))
     .on('error', reject)
     .saveToFile(output);
 });
